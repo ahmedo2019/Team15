e604dc16dece3dd6a90b75719421ae0e9167f046
adarshakanel <52978035+adarshakanel@users.noreply.github.com>
2019-07-22 20:14:32 -0600

    Game and grid

--- Game.java
+++ Game.java
@@ -1,39 +1,45 @@
-import java.util.ArrayList;
-import java.util.Random;
+import java.util.Scanner;
+import java.util.*;
+import java.lang.Math;
+import java.util.Arrays;
 
-public class Game {
+public class Game{
+public static void main(String[] args) {
+    	System.out.println("\n******Welcome to the Card Match Memory Game******\n");
+		System.out.print("Enter 1 for Easy Level or Enter 2 for Hard Level: ");
+	    Scanner input = new Scanner(System.in);
+	    int levelNum = input.nextInt(); 
+	    
+	    if (levelNum == 1 || levelNum == 2) {
+	    	if (levelNum == 1) {
+		    	System.out.println("\nLEVEL DIFFICULTY: EASY :\n");
 
-	public static void main(String[]args) {
-		
-		
-		// Variables 
-		int x = 6; 
-		int number;
-		
-		// Number Generator 
-		
-		Random numberGenerator = new Random();  
-		number = numberGenerator.nextInt(x); // Generations numbers up to X - 1. 
-		
-		System.out.println("Random Number Generated: " + number);
-		
-		String n = Integer.toString(number);
-		
-		String [][] Position = {{n,n,"3","6"},
-								{"4","1","2","3"},
-								{"4","5","6","5"}};
-		
-		for (int i = 0; i < 3; i++) { 			//Tracks outer Array. 
-			for (int j = 0; j < 4; j++) {		//Tracks inner Array. 
-				
-				System.out.print(Position[i][j]+" ");
+	    		Grid.gridPlayEasy();
+	    	}
+	    	else if (levelNum == 2) {
+		    	System.out.println("\nLEVEL DIFFICULTY: HARD :\n");
+
+	    		Grid.gridPlayHard();
+	    		
+	    	}
+	    }
+	    else if (levelNum < 1 || levelNum >2) {
+	    	while (levelNum < 1 || levelNum > 2) {
+	    	System.out.println("\nPlease Choose a valid level of difficulty");
+			System.out.print("\nEnter 1 for Easy Level or Enter 2 for Hard Level: ");
+		    Scanner inputTwo = new Scanner(System.in);
+		    levelNum = inputTwo.nextInt(); 
+		    
+		    if (levelNum == 1) {
+	    	System.out.println("\nLEVEL DIFFICULTY: EASY :\n");
+	    	Grid.gridPlayEasy();
 			}
-			
-			System.out.println();
-		}
-
-
+		    
+		    else if(levelNum == 2) {
+	    	System.out.println("\nLEVEL DIFFICULTY: HARD :\n");
+	    	Grid.gridPlayHard();
+			}
+	    }
 	}
-
 }
-	
+}
