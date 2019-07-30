b0217aa0acfff690243e53ab9b52a571d1afb8b8
ahmedo2019 <ahmedobadca@gmail.com>
2019-07-30 14:25:04 -0600

    contributer:Ahmed Obad

---
+++ Game.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.util.*;
+import java.lang.Math;
+import java.util.Arrays;
+
+public class Game{
+	
+public static void main(String[] args) {
+	// will execute the game by calling the class Multiplay from the file Multiplay
+	// which will run the game inputs and sequences 
+	newMenu.main(args);
+	Multiplayer.Multiplay();
+
+	}
+}
